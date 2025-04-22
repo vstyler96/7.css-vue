3eed0a0e3c9e3b1ddb33200418c102ce4809ca0a
Vincent GS <vstyler96@gmail.com>
2025-04-21 23:06:54 -0600

    :ant: fix(exposed styles): Missing exposed styles

--- dev/App.vue
+++ dev/App.vue
@@ -10,11 +10,8 @@
       maximizable
       minimizable
       color="#4580c4"
-      has-space
     >
-      This is a draggable window XD
-      <br>
-      Hello world!
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
     </WinWindow>
 
     <WinWindow
@@ -32,7 +29,6 @@
     <div class="grid grid-cols-2 gap-3">
       <div>
         <WinWindow
-          has-space
           active
           color="#AC0048"
         >
@@ -54,7 +50,6 @@
     </div>
 
     <WinWindow
-      has-space
       active
       color="pink"
     >
